d32b48e8eba9bbc2506b2e7fe9d57b8f77d919ee
AriFlo <ariflo@arianflores.com>
2015-11-04 14:36:23 -0800

    test

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 		var blksquare = document.createElement("div");
 		body.appendChild(blksquare); 
 
-		blksquare.style.background = "black";
+		blksquare.style.background = "#00000i";
 		blksquare.style.width = '11.1%';
 		blksquare.style.float = 'left';
 		blksquare.style.paddingBottom = '11.1%';
@@ -13,7 +13,7 @@
 		var redsquare = document.createElement("div");
 		body.appendChild(redsquare); 
 
-		redsquare.style.background = "red";
+		redsquare.style.background = "#i00000";
 		redsquare.style.width = '11.1%';
 		redsquare.style.float = 'left';
 		redsquare.style.paddingBottom = '11.1%';
