f708367aca13c29e879b1b58ea58898bd348fe8f
AriFlo <ariflo@arianflores.com>
2015-11-04 14:27:25 -0800

    finished checkerboard excercise

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// Your JS goes here
+	var body = document.getElementsByTagName("body") [0];
+
+	for(var i = 0; i<=99; i++){
+
+		var blksquare = document.createElement("div");
+		body.appendChild(blksquare); 
+
+		blksquare.style.background = "black";
+		blksquare.style.width = '11.1%';
+		blksquare.style.float = 'left';
+		blksquare.style.paddingBottom = '11.1%';
+
+		var redsquare = document.createElement("div");
+		body.appendChild(redsquare); 
+
+		redsquare.style.background = "red";
+		redsquare.style.width = '11.1%';
+		redsquare.style.float = 'left';
+		redsquare.style.paddingBottom = '11.1%';
+
+	}
+
+
+
+
+
+
+
+
+ 
